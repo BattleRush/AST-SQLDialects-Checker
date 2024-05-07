5f67eb50dee89918a8493072cdccedbd5cd9952e
Karlo Piskor <kpiskor@student.ethz.ch>
2024-05-07 17:30:57 +0200

    sqlite parser

--- parse.py
+++ parse.py
@@ -4,24 +4,112 @@ import os
 import json
 
 def parse_sqlite():
-    # check if input/sqlite exists 
-    if not os.path.exists("input/sqlite"):
-        os.makedirs("input/sqlite")
+    
+    log_file = "databases/sqlite/testrunner.log"
+    
+
+    # in the log file the tests are structured like this
+    # ----START func7-200
+    # >>>>
+    # SELECT round(ln(5),2), log(100.0), log(100), log(2,'256');
+    # <<<<
+    # ;;;;
+    # 1.61 2.0 2.0 8.0
+    # ::::
+    # ----END
+    
+    # open the file and extract the sql query, expected result and the name of the test which is after START
+    
+    # delete the input/sqlite folder
+    if os.path.exists("input/sqlite"):
+        os.system("rm -rf input/sqlite")
         
-    # go into databases/sqlite/test folder and get every .test file
+    # create the input/sqlite folder
+    os.makedirs("input/sqlite")
     
-    files = os.listdir("databases/sqlite/test")
-    
-    for file in files:
-        if file.endswith(".test"):
-            print(f"[-] Parsing {file} ...", end="")
-            with open(f"databases/sqlite/test/{file}", "r") as f:
-                lines = f.readlines()
-                # create a new file in input/sqlite with the same name
-                with open(f"input/sqlite/{file}", "w") as f2:
-                    for line in lines:
-                        f2.write(line)
+    with open(log_file, 'r') as file:
+        lines = file.readlines()
+        current_test = None
+        is_query = False
+        is_output = False
+        
+        current_main_test = None
+        
+        all_tests = []
+        
+        for line in lines:
+            line = line.strip()
+            if not line or line.startswith("#"):
+                if not line:
+                    is_query = False
+                    is_output = False
+                continue
+            elif line.startswith("----START"):              
+                test_name = line.split()[1]
+                #print("Test name: ", test_name)
+                
+                # split by - and before this is the main test
+                current_main_test = test_name.split("-")[0]
+            
+                if current_test is not None:
+                    # check if all_test contains the current_main_test as name
+                    found = False
+                    for test in all_tests:
+                        if test["name"] == current_main_test:
+                            found = True
+                            break
                         
+                    if not found:
+                        all_tests.append({"name": current_main_test, "tests": [current_test]})
+                    else:
+                        for test in all_tests:
+                            if test["name"] == current_main_test:
+                                test["tests"].append(current_test)
+                                break
+                
+                current_test = {"name": line.split()[1], "query": "", "expected_result": ""}
+            elif line.startswith("----END"):
+                is_query = False
+                is_output = False
+            elif line.startswith(">>>>"):
+                is_query = True
+            elif line.startswith("<<<<"):
+                is_query = False
+            elif line.startswith(";;;;"):
+                is_output = True
+            elif line.startswith("::::"):
+                is_output = False
+            else:
+                if is_query:
+                    current_test["query"] += line + "\n"
+                elif is_output:
+                    current_test["expected_result"] += line + "\n"
+                else:
+                    #raise ValueError(f"Unexpected line: {line}")
+                    pass # skip the line
+
+        # save remaining test
+        if current_test is not None:
+            # check if all_test contains the current_main_test as name
+            found = False
+            for test in all_tests:
+                if test["name"] == current_main_test:
+                    found = True
+                    break
+
+            if not found:
+                all_tests.append({"name": current_main_test, "tests": [current_test]})
+            else:
+                for test in all_tests:
+                    if test["name"] == current_main_test:
+                        test["tests"].append(current_test)
+                        break
+        
+        # save all tests in a json file
+        for test in all_tests:
+            print("Test: ", test)
+            with open("input/sqlite/" + test["name"] + ".json", "w") as f:
+                f.write(json.dumps(test, indent=4))                 
                         
 def parse_postgresql():
     # check if input/postgresql exists 
@@ -204,4 +292,5 @@ def parse_duckdb():
     print ("Success:", success, " Fail:", fail)
     #print(parsed_tests)
 
-parse_duckdb()
+#parse_duckdb()
+parse_sqlite()
